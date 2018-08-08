e8f559e4686decc93ffae7582f6dc18e0d19e17f
Alexjairo <jairomorocho@gmail.com>
2018-08-08 00:59:44 -0500

    COMPLETO

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function calcular() {
-
-    let n1 = document.getElementById("numero1").value;
-    let respuesta = "";
-
-    for(let i = 1; i = n1; i++){
-        n1=n1-1;
-        respuesta = respuesta + (n1*3)  + ", " ;
+function calcular(){
+    let  n2 =parseInt( document.getElementById("elementos").value);
+    let  n1 =parseInt( document.getElementById("numero1").value);
+    let  n3 =0;
+    respuesta="";
+    
+    for(let i= 1; i<=n2;i++){
+      respuesta=respuesta+""+n1+", ";
+      n3=n1;
+    
+      n1=n3 *3 ;
     }
 
-
-
-
-
-
-
-    document.getElementById("respuesta").innerHTML = respuesta;
-}
+    document.getElementById("respuesta").innerHTML = respuesta;     
+  
+  }
+   
+  
